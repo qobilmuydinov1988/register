52f1a16610f45b5ab04f421c5d5b20c0597be513
Qobil <qobilmuydinov200@gmail.com>
2025-09-18 13:11:03 +0500

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+/*import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
 import SignIn from "./pages/SignIn";
@@ -46,6 +46,59 @@ function App() {
   );
 
   return <div>{readyAuth && <RouterProvider router={routes} />}</div>;
+}
+
+export default App;
+*/
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+import SignIn from "./pages/SignIn";
+import { auth } from "./firebase/firebase-config";
+import SignUp from "./pages/SignUp";
+import { useEffect } from "react";
+import { useGlobalcontext } from "./hooks/useGlobalcontext";
+import { onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const { user, dispatch, readyAuth } = useGlobalcontext();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(`onAuthStateChanged malumoti ${user}`);
+      dispatch({ type: "LOGIN", payload: user });
+      dispatch({ type: "AUTH_READY" });
+    });
+  }, []);
+
+  return (
+    <div>
+      {readyAuth && (
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route
+                index
+                element={user ? <Home /> : <Navigate to="/signin" replace />}
+              />
+            </Route>
+
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" replace /> : <SignUp />}
+            />
+            <Route
+              path="/signin"
+              element={user ? <Navigate to="/" replace /> : <SignIn />}
+            />
+
+            {/* fallback */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </HashRouter>
+      )}
+    </div>
+  );
 }
 
 export default App;
